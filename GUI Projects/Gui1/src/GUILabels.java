dc714ac03b0751cc6c4215ab0cf6af282b297619
GeryMa <117230570+GeryMa@users.noreply.github.com>
2022-11-09 12:29:39 -0500

    Experimenting in class with GUI

---
+++ GUI Projects/Gui1/src/GUILabels.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+import java.awt.*;
+public class GUILabels extends JFrame{
+    public GUILabels(){
+        setTitle("JLabels");
+        setSize(300,300);
+        
+        //the words we will arrange on the pane
+        JLabel a = new JLabel("Hello");
+        JLabel b = new JLabel("World");
+        JLabel c = new JLabel("Whats");
+        JLabel d = new JLabel("Up");
+        
+        JPanel panel1 = new JPanel();
+		panel1.setBackground(Color.ORANGE);
+        a.setAlignmentX(20);
+        Container pane = this.getContentPane();//where we will place the labels
+        pane.setLayout(new FlowLayout());
+        pane.add(a);
+        pane.add(b);
+        pane.add(c);
+        pane.add(d);
+        pane.add(panel1);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+    public static void main(String[] args){
+        GUILabels GUIWindow = new GUILabels();
+    }
+}
+
+
--- GUI Projects/Gui1/src/GUITest.java
+++ GUI Projects/Gui1/src/GUITest.java
@@ -5,12 +5,29 @@ public class GUITest{
 	
 	public static void main (String[] args) {
 		JFrame theGUI = new JFrame();
-		theGUI.setTitle("GUI Program");
+		theGUI.setTitle("Four Colors");
 		theGUI.setSize(300,300);
 		theGUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		ColorPanel panel = new ColorPanel(Color.white);
 		Container pane = theGUI.getContentPane();
-		pane.add(panel);
+		pane.setLayout(new GridLayout(2,2));
+
+		JPanel panel1 = new JPanel();
+		panel1.setBackground(Color.ORANGE);
+
+		JPanel panel2 = new JPanel();
+		panel2.setBackground(Color.BLACK);
+
+		JPanel panel3 = new JPanel();
+		panel3.setBackground(Color.GREEN);
+		
+		JPanel panel4 = new JPanel();
+		panel4.setBackground(Color.WHITE);
+
+		theGUI.add(panel1);
+		theGUI.add(panel2);
+		//theGUI.add(panel3);
+		theGUI.add(panel4);
+
 		theGUI.setVisible(true);
 	}
 }
